5f956597d8796afbb4345e4bf7aa9d865386b14e
wangwei <370795018@qq.com>
2019-11-16 09:27:29 +0800

    Initial commit

---
+++ manage-system-config/src/main/java/com/wwmust/manage/system/config/response/JsonResult.java
@@ -0,0 +1,18 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2019 wwmust.com. All Rights Reserved
+ * This file is part of wwmust EA project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package com.wwmust.manage.system.config.response;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author 37079<37079@wwmust.com>
+ * @date 11/11/2019 19:56
+ */
+public class JsonResult<T> {
+}
---
+++ manage-system-facade/src/main/java/com/wwmust/manage/system/facade/param/TestParam.java
@@ -0,0 +1,18 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2019 wwmust.com. All Rights Reserved
+ * This file is part of wwmust project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package com.wwmust.manage.system.facade.param;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author 37079<37079@wwmust.com>
+ * @date 11/11/2019 16:05
+ */
+public class TestParam {
+}

f577b80da5f1aa370f0de801c8ed6dd544cd8048
wangwei <370795018@qq.com>
2019-11-22 08:18:55 +0800

    环境搭建

--- manage-system-starter/src/main/java/com/wwmust/manage/system/starter/ManageSystemSpringBootApplication.java
+++ manage-system-starter/src/main/java/com/wwmust/manage/system/starter/ManageSystemSpringBootApplication.java
@@ -8,9 +8,11 @@
  **/
 package com.wwmust.manage.system.starter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -22,13 +24,26 @@ import org.springframework.stereotype.Service;
  */
 @SpringBootApplication(scanBasePackages = {
         "com.wwmust.manage.system.service",
-        "com.wwmust.manage.system.starter",
-        "com.wwmust.manage.system.dao.*"
+        "com.wwmust.manage.system.starter"
 })
-@Component("com.wwmust.manage.system.service.user.impl")
-@MapperScan("com.wwmust.manage.system.dao")
+@MapperScan(basePackages = {
+        "com.wwmust.manage.system.dao"
+})
+@Slf4j
 public class ManageSystemSpringBootApplication {
-        public static void main(String[] args) {
-            SpringApplication.run(ManageSystemSpringBootApplication.class, args);
-        }
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ManageSystemSpringBootApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "服务端 '{}' 启动成功!" +
+                        "\n\t环境: \t{}" +
+                        "\n\t访问地址: \thttp://127.0.0.1:{}" +
+                        "\n\t接口文档: \thttp://127.0.0.1:{}/swagger-ui.html" +
+                        "\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getActiveProfiles(),
+                env.getProperty("server.port"),
+                env.getProperty("server.port")
+        );
     }
+}

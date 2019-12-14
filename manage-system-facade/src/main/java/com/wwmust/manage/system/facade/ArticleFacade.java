7f6e79ccad406afb7c9892102b646ddf9d76e7bc
wangwei <370795018@qq.com>
2019-12-14 20:55:22 +0800

    发布接口实现

---
+++ manage-system-facade/src/main/java/com/wwmust/manage/system/facade/ArticleFacade.java
@@ -0,0 +1,45 @@
+/**
+ * *****************************************************
+ * Copyright (C) 2019 wwmust.com. All Rights Reserved
+ * This file is part of wwmust project.
+ * Unauthorized copy of this file, via any medium is strictly prohibited.
+ * Proprietary and Confidential.
+ * ****************************************************
+ **/
+package com.wwmust.manage.system.facade;
+
+import com.wwmust.manage.system.facade.param.article.ArticleParam;
+import com.wwmust.manage.system.facade.param.article.ArticleQueryParam;
+import com.wwmust.manage.system.facade.resp.article.ArticleResp;
+import com.wwmust.manage.system.facade.resp.article.ArticleSkinStypeResp;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author wangwei<wwfdqc@126.com>
+ * @date 12/07/2019 15:19
+ */
+public interface ArticleFacade {
+    /**
+     * 获取发布暂存模板皮肤
+     * @return
+     */
+    List<ArticleSkinStypeResp> getSkinType(Long articleSkinTypeId);
+
+    /**
+     * 文章保存及修改
+     * @param param
+     * @return
+     */
+    Map<String,Long> save(ArticleParam param);
+
+    /**
+     * 查询文章集合
+     * @param param
+     * @return
+     */
+    ArticleResp list(ArticleQueryParam param);
+}
---
+++ manage-system-generaor/src/main/java/com/wwmust/manage/system/model/Article.java
@@ -0,0 +1,182 @@
+package com.wwmust.manage.system.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.*;
+
+public class Article {
+    @Id
+    @Column(name = "article_id")
+    private Long articleId;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "skin_id")
+    private Long skinId;
+
+    @Column(name = "create_user")
+    private String createUser;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_user")
+    private String updateUser;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    @Column(name = "topic_id")
+    private Long topicId;
+
+    @Column(name = "is_default")
+    private String isDefault;
+
+    @Column(name = "is_hot")
+    private String isHot;
+
+    @Column(name = "is_sensitive")
+    private String isSensitive;
+
+    private BigDecimal longitude;
+
+    private BigDecimal latitude;
+
+    @Column(name = "is_public")
+    private String isPublic;
+
+    @Column(name = "is_draft")
+    private String isDraft;
+
+    @Column(name = "article_content")
+    private String articleContent;
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Long getSkinId() {
+        return skinId;
+    }
+
+    public void setSkinId(Long skinId) {
+        this.skinId = skinId;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(Long topicId) {
+        this.topicId = topicId;
+    }
+
+    public String getIsDefault() {
+        return isDefault;
+    }
+
+    public void setIsDefault(String isDefault) {
+        this.isDefault = isDefault;
+    }
+
+    public String getIsHot() {
+        return isHot;
+    }
+
+    public void setIsHot(String isHot) {
+        this.isHot = isHot;
+    }
+
+    public String getIsSensitive() {
+        return isSensitive;
+    }
+
+    public void setIsSensitive(String isSensitive) {
+        this.isSensitive = isSensitive;
+    }
+
+    public BigDecimal getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(BigDecimal longitude) {
+        this.longitude = longitude;
+    }
+
+    public BigDecimal getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(BigDecimal latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getIsPublic() {
+        return isPublic;
+    }
+
+    public void setIsPublic(String isPublic) {
+        this.isPublic = isPublic;
+    }
+
+    public String getIsDraft() {
+        return isDraft;
+    }
+
+    public void setIsDraft(String isDraft) {
+        this.isDraft = isDraft;
+    }
+
+    public String getArticleContent() {
+        return articleContent;
+    }
+
+    public void setArticleContent(String articleContent) {
+        this.articleContent = articleContent;
+    }
+}
